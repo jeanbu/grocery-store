b2d24f55f8c3a8ff975f80adb76d61ae6b70e54d
jean <jean.bustinza@gmail.com>
2022-11-23 11:22:54 +0000

    half of the app done

---
+++ src/LineItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+  return (
+    <li className="item">
+      <input
+        type="checkbox"
+        onChange={() => handleCheck(item.id)}
+        checked={item.checked}
+      />
+      <label onDoubleClick={() => handleCheck(item.id)}>{item.item}</label>
+      <button onClick={() => handleDelete(item.id)}>X</button>
+    </li>
+  );
+};
+
+export default LineItem;
